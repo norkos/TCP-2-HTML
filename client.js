166193673ca6ec555ed1a3abecd1d012519fbc57
norkos <norbert.kosmal@gmail.com>
2015-10-03 22:32:26 +0200

    Prompt added to client

--- client.js
+++ client.js
@@ -1,28 +1,19 @@
 var net = require('net');
+var readline = require('readline');
 
 var HOST = '127.0.0.1';
 var PORT = 6969;
 
 var client = new net.Socket();
-client.connect(PORT, HOST, function() {
+var rl = readline.createInterface(process.stdin, process.stdout);
 
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
-    client.write('I am Chuck Norris!');
+rl.setPrompt('OHAI> ');
+rl.prompt();
 
-});
+rl.on('line', function(line) {
+	client.connect(PORT, HOST, function() {
+	    client.write(line.trim());
+	});
 
-// Add a 'data' event handler for the client socket
-// data is what the server sent to this socket
-client.on('data', function(data) {
-    
-    console.log('DATA: ' + data);
-    // Close the client socket completely
-    client.destroy();
-    
-});
-
-// Add a 'close' event handler for the client socket
-client.on('close', function() {
-    console.log('Connection closed');
+	rl.prompt();
 });
